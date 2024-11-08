e8f886b4766ba0a1d40f7c0b2a365b95f58dd3e7
Juan Aguinaga <juan_a03@icloud.com>
2024-11-07 23:13:38 -0600

    added the delete function to Card

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,28 +1,32 @@
-import React from 'react'
-import FeedbackItem from './FeedbackItem';
-import PropTypes from 'prop-types';
+import React from "react";
+import FeedbackItem from "./FeedbackItem";
+import PropTypes from "prop-types";
 
-function FeedbackList({feedback}) {
-   if (!feedback || feedback.length === 0) {
-      return 'No feedback yet'
-   }
+function FeedbackList({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) {
+    return "No feedback yet";
+  }
   return (
-    <div className='feedback-list'>
+    <div className="feedback-list">
       {feedback.map((item) => (
-         <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem
+          key={item.id}
+          item={item}
+          handleDelete={handleDelete}
+        />
       ))}
     </div>
-  )
+  );
 }
 
 FeedbackList.propTypes = {
-   feedback: PropTypes.arrayOf(
-      PropTypes.shape({
-         id: PropTypes.number.isRequired,
-         text: PropTypes.string.isRequired,
-         rating: PropTypes.number.isRequired
-      })
-   )
-}
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
 
-export default FeedbackList
+export default FeedbackList;
